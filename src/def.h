a7945a37e74b7c00978672872285300c95c9ca82
Roland4444 <turbopollux@rambler.ru>
2019-02-28 16:58:00 +0400

    update files

---
+++ src/def.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <string.h>
+
+typedef struct {
+  void (*func)(void);
+} Class;
+
+
+typedef void (*load__)(Class*);
+typedef Class* (*init__)(void);
