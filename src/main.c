e5340bbb15a331a95dc3805eb06218aa44bac1c0
Roland4444 <turbopollux@rambler.ru>
2019-03-01 14:03:56 +0400

    try inc

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
 #include "def.h"
 
-void func_(){
+int i=0;
+
+void get_i()
+{
+  printf("\n\n%d\n\n", i);
+
+}
+
+void inc_i()
+{
+  i++;
+}
+void func_()
+{
   printf("\n\n\nCalled succesfull!\n\n");
 }
-Class*  init(){
+
+Class*  init()
+{
   Class* ptr = (Class*)malloc(sizeof(Class));
   ptr -> func = func_;
   return ptr;
@@ -14,7 +29,8 @@ void call(Class* ptr)
   ptr->func();
 }
 
-int main(void) {
+int main(void)
+{
   call(init());
   call(NULL);
 }
